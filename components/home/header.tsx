2443ca3a1ae7e65277796e75dcd05e6acab0400b
Arinze-Eze <emmanuelrinz@gmail.com>
2025-05-17 15:08:53 +0100

    connected supabase and created a modal for the sign-in

--- components/home/header.tsx
+++ components/home/header.tsx
@@ -1,10 +1,15 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { ModeToggle } from "../toogle-theme";
+import { Button } from "../ui/button";
+import Modal from "../modal";
+import SignIn from "../authentication-ui/auth";
 
 function Header() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <section className="flex content-center justify-between p-4 sticky top-0 z-50 bg-background border-b">
       <div className="font-semibold text-2xl">
@@ -16,6 +21,16 @@ function Header() {
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
+
+        <Button onClick={() => setIsModalOpen(true)}>Sign in</Button>
+
+        <Modal
+          title="Sign in to your leetCoach Account"
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+        >
+          <SignIn />
+        </Modal>
       </div>
     </section>
   );
---
+++ utils/constants/env.ts
@@ -0,0 +1,2 @@
+export const SUPABASE_URL = process.env.SUPABASE_URL as string;
+export const SUPABASE_ANON = process.env.SUPABASE_ANON as string;
